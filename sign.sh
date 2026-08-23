cab7fe27022711b0bf42d1b2456321d25340261f
Samuel García Salas <12875783+samuelwtheck@users.noreply.github.com>
2026-08-23 12:53:41 +0200

    sign.sh: commented-out instruction for signing psbt

--- sign.sh
+++ sign.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 # TODO:
 # - Interactively ask for the seed phrase
 # - Load wallet using provided seed phrase
 # - Pass psbt file to be signed
 # - Output the signature
+
+#electrum --offline --mock-wallet-path --seed "your twelve or twenty four word seed phrase here" signtransaction "$(cat partially_signed.txn)" > signed.txn
