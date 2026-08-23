ff50f80a979a72ec0fcea256cbfbe6a5702e21b9
Samuel García Salas <12875783+samuelwtheck@users.noreply.github.com>
2026-08-23 23:28:05 +0200

    Allow calling build.sh from whatever current working directory

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir -vp ./mkosi.extra/usr/local/bin
+this_script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+mkdir -vp $this_script_dir/mkosi.extra/usr/local/bin
 
 download() {
 	local url="$1"
@@ -14,7 +16,7 @@ GETH_FILE_NAME="geth-alltools-linux-amd64-1.17.4-36a7dc72.tar.gz"
 GETH_URL="https://gethstore.blob.core.windows.net/builds/$GETH_FILE_NAME"
 
 download "$GETH_URL"
-tar -xvzf "/tmp/$GETH_FILE_NAME" -C ./mkosi.extra/usr/local/bin/
+tar -xvzf "/tmp/$GETH_FILE_NAME" -C $this_script_dir/mkosi.extra/usr/local/bin/
 
 # Bitcoin
 
@@ -22,19 +24,19 @@ ELECTRUM_FILE_NAME="electrum-4.7.2-x86_64.AppImage"
 ELECTRUM_URL="https://download.electrum.org/4.7.2/$ELECTRUM_FILE_NAME"
 
 download "$ELECTRUM_URL"
-cp -v "/tmp/$ELECTRUM_FILE_NAME" ./mkosi.extra/usr/local/bin
-chmod +x ./mkosi.extra/usr/local/bin/$ELECTRUM_FILE_NAME
-ln -vfs ./$ELECTRUM_FILE_NAME ./mkosi.extra/usr/local/bin/electrum
+cp -v "/tmp/$ELECTRUM_FILE_NAME" $this_script_dir/mkosi.extra/usr/local/bin
+chmod +x $this_script_dir/mkosi.extra/usr/local/bin/$ELECTRUM_FILE_NAME
+ln -vfs ./$ELECTRUM_FILE_NAME $this_script_dir/mkosi.extra/usr/local/bin/electrum
 
 # Helper scripts
 
-install -v ./cryptoscripts/balance.sh ./mkosi.extra/usr/local/bin/
-install -v ./cryptoscripts/sign.sh    ./mkosi.extra/usr/local/bin/
+install -v $this_script_dir/cryptoscripts/balance.sh $this_script_dir/mkosi.extra/usr/local/bin/
+install -v $this_script_dir/cryptoscripts/sign.sh    $this_script_dir/mkosi.extra/usr/local/bin/
 
 # Service presets
 
-mkdir -vp ./mkosi.extra/usr/lib/systemd/system-preset
-cp -v ./systemd.preset ./mkosi.extra/usr/lib/systemd/system-preset/90-custom.preset
+mkdir -vp this_script_dir/mkosi.extra/usr/lib/systemd/system-preset
+cp -v $this_script_dir/systemd.preset $this_script_dir/mkosi.extra/usr/lib/systemd/system-preset/90-custom.preset
 
 # mkosi
 
